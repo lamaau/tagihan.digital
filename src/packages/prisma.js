d807195d282f88feb404dc3117a4321f0c075f52
rizkhal <lamaaurizkhal@gmail.com>
2023-01-14 17:02:00 +0800

    fix: typo

--- src/packages/prisma.js
+++ src/packages/prisma.js
@@ -1,20 +1,24 @@
 let prisma;
 
-if (import.meta.env.MODE === "development") {
-  import("@prisma/client/index.js").then(
-    (mod) => (prisma = new mod.PrismaClient())
-  );
-} else {
-  import("@prisma/client/edge.js").then(
-    (mod) =>
-      (prisma = new mod.PrismaClient({
-        datasources: {
-          db: {
-            url: import.meta.env.DATABASE_URL,
-          },
-        },
-      }))
-  );
-}
+import("@prisma/client/index.js").then(
+  (mod) => (prisma = new mod.PrismaClient())
+);
+
+// if (import.meta.env.MODE === "development") {
+//   import("@prisma/client/index.js").then(
+//     (mod) => (prisma = new mod.PrismaClient())
+//   );
+// } else {
+//   import("@prisma/client/edge.js").then(
+//     (mod) =>
+//       (prisma = new mod.PrismaClient({
+//         datasources: {
+//           db: {
+//             url: import.meta.env.DATABASE_URL,
+//           },
+//         },
+//       }))
+//   );
+// }
 
 export { prisma };
